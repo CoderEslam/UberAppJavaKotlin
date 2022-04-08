f347340f5ccdf22b732d2b0ecf662e1bffda319d
eslam <eslamfgazy600@gmail.com>
2022-04-08 20:29:07 +0200

    SignUp&SignIn

--- app/src/main/java/com/doubleclick/uberappjavakotlin/ui/main/SplachActivity.java
+++ app/src/main/java/com/doubleclick/uberappjavakotlin/ui/main/SplachActivity.java
@@ -19,25 +19,25 @@ public class SplachActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splach);
-        WebView webView = findViewById(R.id.web);
-        webView.getSettings().setAllowContentAccess(true);
-        webView.getSettings().setAllowFileAccess(true);
-        WebSettings settings = webView.getSettings();
+//        WebView webView = findViewById(R.id.web);
+//        webView.getSettings().setAllowContentAccess(true);
+//        webView.getSettings().setAllowFileAccess(true);
+//        WebSettings settings = webView.getSettings();
 //        settings.setJavaScriptEnabled(true);
-        settings.setAllowFileAccess(true);
+//        settings.setAllowFileAccess(true);
 //        webView.loadUrl("https://www.tutorialspoint.com/android/index.htm");
-        webView.loadUrl("file:///android_asset/index.html");
+//        webView.loadUrl("file:///android_asset/index.html");
 
-//        MediaPlayer player = MediaPlayer.create(SplachActivity.this, R.raw.hobk);
-//        player.start();
-//        new Handler().postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                /* Create an Intent that will start the Menu-Activity. */
-//                Intent mainIntent = new Intent(SplachActivity.this, MainActivity.class);
-//                startActivity(mainIntent);
-//                finish();
-//            }
-//        }, 2000);
+        MediaPlayer player = MediaPlayer.create(SplachActivity.this, R.raw.hobk);
+        player.start();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplachActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, 2000);
     }
 }
